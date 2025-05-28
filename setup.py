ef98fae08648d4ce0b21bf22c8874675b8134814
PravinSK <pravinsk87@gmail.com>
2025-05-28 18:25:42 +0530

    Version2

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 def get_requirements_list()->List[str]:
     with open("requirements.txt") as req_file:
-        return req_file.readlines()
+        return req_file.readlines().remove("-e .")
 
 setup(
     name    = "Housing Predictor",
     version = "0.0.1",
     author  = "Pravin SK",
     description = "House price prediction project",
-    packages = ["Housing"],
+    packages = find_packages(),
     install_requires = get_requirements_list()
 )
 
