f38861465dbc4701bfe789dca7ca582f2ff11201
PravinSK <pravinsk87@gmail.com>
2025-05-29 12:54:20 +0530

    Logger and exception added

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from Housing.logger import logging
+from Housing.exception import housing_exp
+import os, sys
 
 
 app=Flask(__name__)
@@ -6,6 +9,12 @@ app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:       
+       housing =  housing_exp(e,sys)
+       logging.info(housing.error_message)
+    logging.info("We are testing logging module")
     return "My First Machine learning projecct"
 
 
